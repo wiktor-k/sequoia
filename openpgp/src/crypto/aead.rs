12d6dd030bdc4423c147f78d3d73fa0ee1d43acd
Justus Winter <justus@sequoia-pgp.org>
2022-01-10 16:26:26 +0100

    openpgp: Remove redundant cipher op parameter.
      - The decryptor only decrypts, the encryptor only encrypts.  No need
        to have that parameter (in fact, having the parameter presents the
        opportunity to get it wrong, see the previous commit).

--- openpgp/src/crypto/aead.rs
+++ openpgp/src/crypto/aead.rs
@@ -193,7 +193,7 @@ impl<'a> Decryptor<'a> {
         }
     }
 
-    fn make_aead(&mut self, op: CipherOp) -> Result<Box<dyn Aead>> {
+    fn make_aead(&mut self) -> Result<Box<dyn Aead>> {
         // The chunk index is XORed into the IV.
         let chunk_index: [u8; 8] = self.chunk_index.to_be_bytes();
 
@@ -214,7 +214,8 @@ impl<'a> Decryptor<'a> {
                 }
 
                 // Instantiate the AEAD cipher.
-                let aead = self.aead.context(self.sym_algo, &self.key, &self.iv, op)?;
+                let aead = self.aead.context(self.sym_algo, &self.key, &self.iv,
+                                             CipherOp::Decrypt)?;
 
                 // Restore the IV.
                 for (i, o) in &mut self.iv[iv_len - 8..].iter_mut()
@@ -290,7 +291,7 @@ impl<'a> Decryptor<'a> {
         let final_digest_size = self.digest_size;
 
         for _ in 0..n_chunks {
-            let mut aead = self.make_aead(CipherOp::Decrypt)?;
+            let mut aead = self.make_aead()?;
             // Digest the associated data.
             self.hash_associated_data(&mut aead, false);
 
@@ -381,7 +382,7 @@ impl<'a> Decryptor<'a> {
 
             if check_final_tag {
                 // We read the whole ciphertext, now check the final digest.
-                let mut aead = self.make_aead(CipherOp::Decrypt)?;
+                let mut aead = self.make_aead()?;
                 self.hash_associated_data(&mut aead, true);
 
                 aead.digest(&mut digest);
@@ -614,7 +615,7 @@ impl<W: io::Write> Encryptor<W> {
         }
     }
 
-    fn make_aead(&mut self, op: CipherOp) -> Result<Box<dyn Aead>> {
+    fn make_aead(&mut self) -> Result<Box<dyn Aead>> {
         // The chunk index is XORed into the IV.
         let chunk_index: [u8; 8] = self.chunk_index.to_be_bytes();
 
@@ -635,7 +636,8 @@ impl<W: io::Write> Encryptor<W> {
                 }
 
                 // Instantiate the AEAD cipher.
-                let aead = self.aead.context(self.sym_algo, &self.key, &self.iv, op)?;
+                let aead = self.aead.context(self.sym_algo, &self.key, &self.iv,
+                                             CipherOp::Encrypt)?;
 
                 // Restore the IV.
                 for (i, o) in &mut self.iv[iv_len - 8..].iter_mut()
@@ -667,7 +669,7 @@ impl<W: io::Write> Encryptor<W> {
 
             // And possibly encrypt the chunk.
             if self.buffer.len() == self.chunk_size {
-                let mut aead = self.make_aead(CipherOp::Encrypt)?;
+                let mut aead = self.make_aead()?;
                 self.hash_associated_data(&mut aead, false);
 
                 let inner = self.inner.as_mut().unwrap();
@@ -689,7 +691,7 @@ impl<W: io::Write> Encryptor<W> {
         for chunk in buf.chunks(self.chunk_size) {
             if chunk.len() == self.chunk_size {
                 // Complete chunk.
-                let mut aead = self.make_aead(CipherOp::Encrypt)?;
+                let mut aead = self.make_aead()?;
                 self.hash_associated_data(&mut aead, false);
 
                 let inner = self.inner.as_mut().unwrap();
@@ -717,7 +719,7 @@ impl<W: io::Write> Encryptor<W> {
     pub fn finish(&mut self) -> Result<W> {
         if let Some(mut inner) = self.inner.take() {
             if !self.buffer.is_empty() {
-                let mut aead = self.make_aead(CipherOp::Encrypt)?;
+                let mut aead = self.make_aead()?;
                 self.hash_associated_data(&mut aead, false);
 
                 // Encrypt the chunk.
@@ -735,7 +737,7 @@ impl<W: io::Write> Encryptor<W> {
             }
 
             // Write final digest.
-            let mut aead = self.make_aead(CipherOp::Encrypt)?;
+            let mut aead = self.make_aead()?;
             self.hash_associated_data(&mut aead, true);
             aead.digest(&mut self.scratch[..self.digest_size]);
             inner.write_all(&self.scratch[..self.digest_size])?;
